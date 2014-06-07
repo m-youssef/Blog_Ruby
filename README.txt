d557183cfdd27765f6d43f866d724f18ec273ef4
m_youssef <eng_mahmoud9012@yahoo.com>
2014-06-08 01:22:17 +0300

    Initialize Project

---
+++ README.txt
@@ -0,0 +1,1 @@
+Blog Application using Ruby on Rails 
---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ActiveRecord::Base
+  has_many :comments , dependent: :destroy
+
+
+validates_presence_of :title
+validates_presence_of :body
+
+end
